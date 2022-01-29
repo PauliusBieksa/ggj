76034ff61f3a8881e859d4842f290111b064e173
Daniel Korsah <danielkorsah97@gmial.com>
2022-01-29 07:54:25 +0000

    Crash effects: shrinking Asteroid, slowmo, sound effect, particle effect

--- RedShift BlueShift/Assets/Scripts/Asteroid.cs
+++ RedShift BlueShift/Assets/Scripts/Asteroid.cs
@@ -6,11 +6,11 @@ using Random = UnityEngine.Random;
 
 public class Asteroid : MonoBehaviour
 {
-    Vector3 axisOfRotation;
-
+    private Vector3 axisOfRotation;
     private Vector3 originalSize;
     private Rigidbody rb;
     private ParticleSystem particles;
+    private AudioSource audioSource;
 
 
     // Start is called before the first frame update
@@ -20,6 +20,8 @@ public class Asteroid : MonoBehaviour
 
         originalSize = transform.localScale;
         rb = GetComponent<Rigidbody>();
+        particles = GetComponentInChildren<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -39,17 +41,21 @@ public class Asteroid : MonoBehaviour
         Debug.Log("Crash");
         StartCoroutine("Shrink");
         rb.isKinematic = true;
+        particles.Play();
+        audioSource.Play();
     }
 
     public void Reset()
     {
         transform.localScale = originalSize;
         rb.isKinematic = false;
+        particles.Stop();
     }
 
     IEnumerator Shrink()
     {
         Debug.Log("Shrink");
+        StartCoroutine("SlowMo");
         Vector3 targetScale = new Vector3(0, 0, 0);
         float elapsedTime = 0;
         float waitTime = 0.5f;
@@ -65,5 +71,12 @@ public class Asteroid : MonoBehaviour
         //make sure it getsd there if it's not quite there
         transform.localScale = targetScale;
         yield return null;
+    }
+
+    IEnumerator SlowMo()
+    {
+        Time.timeScale = 0.25f;
+        yield return new WaitForSecondsRealtime(2);
+        Time.timeScale = 1;
     }
 }
